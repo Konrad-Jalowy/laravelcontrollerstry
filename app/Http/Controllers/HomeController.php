3d8ec5828193bba2389f8cea9b0703168a35ed5a
Konrad-Jalowy <sitesystem@wp.pl>
2023-01-31 12:14:39 +0100

    created invokable controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+class HomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+ 
+    return view('welcome', [
+        'tasks' => $tasks
+    ]);
+    }
+}
